494d3d48db8469e0c5de5d1525732ee9bd378ee2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-09-06 15:11:41 +0200

    Validating the value

--- cypress/integration/demos/settings.spec.js
+++ cypress/integration/demos/settings.spec.js
@@ -7,9 +7,9 @@ context('Settings', () => {
   })
 
   it('Change the name to Jack Sparrow', () => {
-    cy.get('#firstName').clear().type('Jack')
-    cy.get('#lastName').type('{selectAll}Sparrow')
-    cy.get('#email').type('captain@black-pearl.ship')
+    cy.get('#firstName').clear().type('Jack').should('have.value', 'Jack')
+    cy.get('#lastName').type('{selectAll}Sparrow').should('have.value', 'Sparrow')
+    cy.get('#email').type('captain@black-pearl.ship').should('have.value', 'captain@black-pearl.ship')
 
     cy.get('#result')
       .should('contain.value', '"firstName": "Jack",')
@@ -20,7 +20,9 @@ context('Settings', () => {
   it('Change the gender to female', () => {
     cy.get('#result').should('contain.value', '"gender": ""')
 
-    cy.get('#female').check()
+    cy.get('#unknown').should('be.checked')
+    cy.get('#female').check().should('be.checked')
+    cy.get('#unknown').should('not.be.checked')
 
     cy.get('#result').should('contain.value', '"gender": "female"')
   })
@@ -28,17 +30,17 @@ context('Settings', () => {
   it('Select the newsletter', () => {
     cy.get('#result').should('contain.value', '"newsletter": false')
     
-    cy.get('#newsletter').check()
+    cy.get('#newsletter').check().should('be.checked')
     cy.get('#result').should('contain.value', '"newsletter": true')
     
-    cy.get('#newsletter').uncheck()
+    cy.get('#newsletter').uncheck().should('not.be.checked')
     cy.get('#result').should('contain.value', '"newsletter": false')
   })
 
   it('Change the country to NL', () => {
     cy.get('#result').should('contain.value', '"country": "US"')
 
-    cy.get('#country').select('Netherlands')
+    cy.get('#country').select('Netherlands').should('have.value', 'NL')
 
     cy.get('#result').should('contain.value', '"country": "NL"')
   })
@@ -46,7 +48,7 @@ context('Settings', () => {
   it('Change the happiness to 95%', () => {
     cy.get('#result').should('contain.value', '"happiness": 80')
 
-    cy.get('#happiness').invoke('val', 95).trigger('input')
+    cy.get('#happiness').invoke('val', 95).trigger('input').should('have.value', '95')
 
     cy.get('#result').should('contain.value', '"happiness": 95')
   })
